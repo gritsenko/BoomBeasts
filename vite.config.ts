20007984d232aa6f31fe14746bc3d3f71c47a22b
Igor Gritsenko <agent190r@yandex.ru>
2025-09-07 01:38:59 +0300

    Updted github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/BoomBeasts/' : '/',
+  base: mode === "production" ? "/BoomBeasts/" : "/",
   server: {
     port: 8080,
     open: true,
